fdadc262e7fdc74b23257367a51997a0854b46d5
Jorge Ramirez <jorgelurd11@outlook.com>
2023-03-23 11:26:23 +0100

    Ya se muestra la base de datos, queda agregarle mejores estilos y los botones

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php $conexion = mysqli_connect('localhost', 'root', '', 'estudiantes'); mysqli_set_charset($conexion,'utf8')?>
+<?php $conexion = mysqli_connect('localhost', 'root', '', 'estudiantes');
+mysqli_set_charset($conexion, 'utf8') ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,27 +8,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet">
     <title>Estudiantes</title>
 </head>
 
 <body>
+    
     <h1>Base de datos: Estudiantes</h1>
 
     <div class="container-tabla">
-        <table class="tabla">
-            <?php $tabla = 'SELECT * FROM informacion'; ?>
-            <tr class="encabezado-tabla">
-                <td>ID</td>
-                <td>Nombre</td>
-                <td>Apellido</td>
-                <td>Cédula</td>
-                <td>Ciudad</td>
-                <td>Teléfono</td>
-                <td>Correo</td>
-                <td>Carrera</td>
-            </tr>
-        </table>
+        <?php $tabla = 'SELECT * FROM informacion'; ?>
+        <div class="tabla">
+            <div class="encabezado">
+                <div class="elemento">Nombre</div>
+                <div class="elemento">Apellido</div>
+                <div class="elemento">Cédula</div>
+                <div class="elemento">Ciudad</div>
+                <div class="elemento">Teléfono</div>
+                <div class="elemento">Correo</div>
+                <div class="elemento">Carrera</div>
+            </div>
+            <div class="cuerpo">
+                <?php $query = mysqli_query($conexion, $tabla);
+
+                while ($registro = mysqli_fetch_assoc($query)) {
+                    echo '<div class="registro">';
+                    foreach ($registro as $dato) {
+                        echo '<div class="elemento">'.$dato.'</div>';
+                    }
+                    echo"</div>";
+                }?>
+            </div>
+        </div>
     </div>
 </body>
-
 </html>
